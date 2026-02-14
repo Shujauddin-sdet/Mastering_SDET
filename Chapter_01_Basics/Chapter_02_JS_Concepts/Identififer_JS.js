359199c018110f5c35c8775d21f9a25ae72931e2
Shujauddin <msshujauddin@gmail.com>
2026-02-14 20:38:13 +0530

    Add Chapter 02 JS Concepts and sync updates

---
+++ Chapter_01_Basics/Chapter_02_JS_Concepts/Identififer_JS.js
@@ -0,0 +1,17 @@
+// Idebtifier , Literal , Operator.
+
+var a = 10;
+a = 20;
+console.log(a);
+
+//Variable name = Identifier (Name which is given to a Container)
+// Literal = Value of a Container
+// Operator = 
+
+//Rules of indentifiers
+// 1. Must start with a letter or an underscore (_)
+// 2. Can contain letters, numbers, and underscores
+// 3. Are case-sensitive
+// 4. Cannot be reserved keywords
+
+
