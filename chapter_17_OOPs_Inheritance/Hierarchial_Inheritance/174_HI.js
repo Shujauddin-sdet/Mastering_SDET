55d9899504837b9f1bdbeabbb407bc04fb09fc04
Shujauddin-sdet <shujauddin.sdet@gmail.com>
2026-06-04 21:58:50 +0530

    feat: implement methods and instantiation for hierarchical inheritance classes in 174_HI.js

--- chapter_17_OOPs_Inheritance/Hierarchial_Inheritance/174_HI.js
+++ chapter_17_OOPs_Inheritance/Hierarchial_Inheritance/174_HI.js
@@ -1,10 +1,25 @@
 class Father {
-
+  sleep() {
+    console.log("Sleeping...");
+  }
 }
 
 class Son1 extends Father {
-
+  walk() {
+    console.log("Walking...");
+  }
 }
 class Son2 extends Father {
-
+  run() {
+    console.log("Need to wake up Father to go for a run...");
+  }
 }
+
+const obj1 = new Son1();
+obj1.sleep();
+obj1.walk();
+
+console.log("---------------------------------------------------");
+const obj2 = new Son2();
+obj2.sleep();
+obj2.run();
